121af22ac2aeb93c5e30e6ed0d2d5971d297ac3e
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-09-12 18:44:32 +0530

    blood requirent routes created

---
+++ api/controllers/blood-requirement.controller.js
@@ -0,0 +1,137 @@
+const mongoose = require('mongoose');
+
+const BloodRequirement = require('../models/blood-requirement.model');
+
+exports.get_my_blood_requirements = (req, res, next) => {
+    const userId = req.userData.userId;
+
+    BloodRequirement
+        .find({ created_by: userId })
+        .exec()
+        .then((bloodRequirements) => {
+            res.status(201).json({
+                success: true,
+                response: bloodRequirements
+            })
+        })
+        .catch((err) => {
+            res.status(201).json({
+                success: false,
+                response: err
+            })
+        })
+};
+
+exports.get_blood_requirement = (req, res, next) => {
+    const userId = req.userData.userId;
+    const id = req.params.id;
+
+    BloodRequirement
+        .findOne({ _id: id, created_by: userId })
+        .exec()
+        .then((bloodRequirement) => {
+            res.status(201).json({
+                success: true,
+                response: bloodRequirement
+            })
+        })
+        .catch((err) => {
+            res.status(201).json({
+                success: false,
+                response: err
+            })
+        })
+}
+
+exports.get_nearby_blood_requirements = (req, res, next) => {
+    const userId = req.userData.userId;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+
+    BloodRequirement
+        .find({
+            active: true,
+            hospital_location: {
+                $near: {
+                    $maxDistance: 1000,
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [latitude, longitude]
+                    }
+                }
+            },
+            created_by: { $ne: userId }
+        })
+        .exec()
+        .then((bloodRequirements) => {
+            res.status(201).json({
+                success: true,
+                response: bloodRequirements
+            })
+        })
+        .catch((err) => {
+            res.status(201).json({
+                success: false,
+                response: err
+            })
+        })
+}
+
+exports.add_new_blood_requirement = (req, res, next) => {
+    const userId = req.userData.userId;
+
+    const bloodRequirement = new BloodRequirement({
+        _id: new mongoose.Types.ObjectId(),
+        blood_group: req.body.blood_group,
+        patient_name: req.body.patient_name,
+        patient_age: req.body.patient_age,
+        hospital_name: req.body.hospital_name,
+        hospital_address: req.body.hospital_address,
+        hospital_location: {
+            type: "Point",
+            coordinates: req.body.hospital_location
+        },
+        required_till: req.body.required_till,
+        documents: req.body.documents,
+        contact_person_name: req.body.contact_person_name,
+        alternate_contact_person_name: req.body.alternate_contact_person_name,
+        created_by: userId,
+        created_at: new Date()
+    })
+
+    bloodRequirement
+        .save()
+        .then(() => {
+            res.status(201).json({
+                success: true,
+                response: 'new blood requirement added'
+            })
+        })
+        .catch((err) => {
+            res.status(201).json({
+                success: false,
+                response: err
+            });
+        })
+}
+
+exports.remove_blood_requirement = (req, res, next) => {
+    const userId = req.userData.userId;
+    const id = req.params.id
+
+    BloodRequirement
+        .update({ _id: id, created_by: userId }, { $set: { "active": false } })
+        .exec()
+        .then(() => {
+            res.status(201).json({
+                success: true,
+                response: 'blood requirement removed'
+            })
+        })
+        .catch((err) => {
+            res.status(201).json({
+                success: false,
+                response: err
+            });
+        })
+}
---
+++ api/models/blood-requirement.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const bloodRequirementModel = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    active: { type: mongoose.Schema.Types.Boolean, default: true },
+    fulfiled: { type: mongoose.Schema.Types.Boolean, default: false },
+    blood_group: { type: mongoose.Schema.Types.ObjectId, ref: 'BloodGroup' },
+    patient_name: { type: String, require: true },
+    patient_age: { type: String, require: true },
+    hospital_name: { type: String, require: true },
+    hospital_address: { type: String, require: true },
+    hospital_location: {
+        type: String,
+        coordinates: []
+    },
+    required_till: { type: mongoose.Schema.Types.Date },
+    documents: [{ type: mongoose.Schema.Types.String }],
+    contact_person_name: { type: mongoose.Schema.Types.String, require: true },
+    alternate_contact_person_name: { type: mongoose.Schema.Types.String },
+    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    created_at: { type: mongoose.Schema.Types.Date }
+})
+
+module.exports = mongoose.model('BloodRequirement', bloodRequirementModel);
