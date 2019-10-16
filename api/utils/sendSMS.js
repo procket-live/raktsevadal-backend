4300276ea6799fd7c71116745538ffbb3a55dfac
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-10-17 04:03:01 +0530

    dsfds

--- api/utils/sendSMS.js
+++ api/utils/sendSMS.js
@@ -1,18 +1,45 @@
-const https = require('https');
-const authKey = '235391AG8E7NoOgG5b8d4843';
+const axios = require('axios');
+const authKey = '235391Ae7cMna4J5da79840';
 
 function sendSMS(mobile, template) {
-    https.get(encodeURI(`https://api.msg91.com/api/sendhttp.php?mobiles=91${mobile}&authkey=${authKey}&route=4&sender=PROCKT&message=${template}&country=91`), (resp) => {
-        let data = '';
+    axios({
+        url: 'https://api.msg91.com/api/v2/sendsms?country=91',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        data: {
+            'sender': 'RKSVDL',
+            'route': '4',
+            'country': '91',
+            'sms': [
+                {
+                    'message': template,
+                    'to': [
+                        mobile
+                    ]
+                }
+            ]
+        },
+        method: 'POST'
+    })
+    // https.request({
+    //     hostname: ,
+    //     method: 'POST',
+    //     body: {
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-            console.log('data', data)
-        });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+    //     }
+    // })
+    // https.get(encodeURI(`https://api.msg91.com/api/sendhttp.php?mobiles=91${mobile}&authkey=${authKey}&route=4&sender=PROCKT&message=${template}&country=91`), (resp) => {
+    //     let data = '';
+
+    //     // A chunk of data has been recieved.
+    //     resp.on('data', (chunk) => {
+    //         data += chunk;
+    //         console.log('data', data)
+    //     });
+    // }).on("error", (err) => {
+    //     console.log("Error: " + err.message);
+    // });
 }
 
 module.exports = sendSMS;
