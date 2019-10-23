23def861aa813f2e31153daf58a9cdfc0dadca89
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-10-23 11:29:12 +0530

    dfsd

--- api/utils/sendSMS.js
+++ api/utils/sendSMS.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const querystring = require('querystring');
 const authKey = '235391Ae7cMna4J5da79840';
 
 function sendSMS(mobile, template) {
@@ -28,4 +29,29 @@ function sendSMS(mobile, template) {
     })
 }
 
-module.exports = sendSMS;
+function sendSMS2(mobile, template) {
+    const params = {
+        mobiles: `91${mobile}`,
+        authkey: authKey,
+        route: 4,
+        sender: 'RKSVDL',
+        message: encodeURIComponent(template),
+        country: 91,
+    };
+
+    const query = querystring.stringify(params);
+
+    const options = {
+        url: `https://api.msg91.com/api/sendhttp.php?${query}`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'GET'
+    };
+
+    axios(options).then((res) => {
+        console.log('rrreesss', res);
+    })
+}
+
+module.exports = sendSMS2;
